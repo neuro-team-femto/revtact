d36d682efb70e093979c7f446291c81daeb13403
JJ Aucouturier <aucouturier@gmail.com>
2022-10-03 06:21:40 +0200

    modified ni_reader to start only once per participant, and switch datafile at each new trial

--- experiment/config/config_nireader_simulated.py
+++ experiment/config/config_nireader_simulated.py
@@ -1,5 +1,5 @@
-params = {'sampling_freq_in': 1000,
-	'buffer_in_size': 100,
+params = {'sampling_freq_in': 1000, # 1000Hz
+	'buffer_in_size': 100, # record a buffer every 100ms
 	'chans_in' : 6,
 	'chans_id': ['x_left','y_left','z_left','x_right','y_right','z_right'],
 	'running' : False,
