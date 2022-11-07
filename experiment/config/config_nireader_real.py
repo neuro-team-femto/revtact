ee84e8a5baef5af7e81ed27a2e6e7b2564c20cb9
JJ Aucouturier <aucouturier@gmail.com>
2022-11-07 20:56:42 +0100

    fixed real channel names in config file

--- experiment/config/config_nireader_real.py
+++ experiment/config/config_nireader_real.py
@@ -1,7 +1,7 @@
 params = {'sampling_freq_in': 5000, # 1000Hz
 	'buffer_in_size': 500, # record a buffer every 100ms
 	'chans_in' : 4,
-	'chans_id': ['f1','f2','ft','acc'],
+	'chans_id': ['fn1','fn2','ft','acc'],
 	'running' : False,
 	'dev' : "dev2/ai0:3"
 }
